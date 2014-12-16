01d28060aa139e8171ec2813cba5f62abbef5b84
Greg Cochard <gcochard@users.noreply.github.com>
2014-12-15 16:46:07 -0800

    chromium

---
+++ chromium/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Greg Cochard"
+license          "apachev2"
+description      "Installs/symlinks chromium"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+
+supports "debian"
