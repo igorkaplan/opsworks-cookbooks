7af08f2f65989652fa51a3e6eeecb7d15b8b3c28
Greg Cochard <gcochard@users.noreply.github.com>
2014-12-15 16:58:01 -0800

    foodcritic review

--- chromium/recipes/default.rb
+++ chromium/recipes/default.rb
@@ -1,19 +1,15 @@
-log "downloading chromium" do
-    execute "download chromium" do
-      command "cd ~#{node[:chromium][:user]} && wget #{node[:chromium][:location]} -O archive.zip"
-    end
+remote_file "/home/#{node['chromium']['user']}/archive.zip" do
+  source node['chromium']['location']
 end
 
-log "installing chromium" do
-  execute "unzip chromium" do
-    command "unzip -d ~#{node[:chromium][:user]}/#{node[:chromium][:expand]} archive.zip && mv ~#{node[:chromium][:user]}/#{node[:chromium][:expand]}/*/* ~#{node[:chromium][:user]}/#{node[:chromium][:expand]}/"
-  end
+execute "unzip chromium" do
+  command "unzip -d ~#{node['chromium']['user']}/#{node['chromium']['expand']} archive.zip && mv ~#{node['chromium']['user']}/#{node['chromium']['expand']}/*/* ~#{node['chromium']['user']}/#{node['chromium']['expand']}/"
+end
 
-  execute "symlink chromium" do
-    command "ln -s ~#{node[:chromium][:user]}/#{node[:chromium][:expand]}/chrome /usr/bin/#{node[:chromium][:symlink]}"
-  end
+execute "symlink chromium" do
+  command "ln -s ~#{node['chromium']['user']}/#{node['chromium']['expand']}/chrome /usr/bin/#{node['chromium']['symlink']}"
+end
 
-  execute "chown/chmod chrome_sandbox" do
-    command "chown root:root ~#{node[:chromium][:user]}/#{node[:chromium][:expand]}/chrome_sandbox && chmod 4755 ~#{node[:chromium][:user]}/#{node[:chromium][:expand]}/chrome_sandbox"
-  end
+execute "chown/chmod chrome_sandbox" do
+  command "chown root:root ~#{node['chromium']['user']}/#{node['chromium']['expand']}/chrome_sandbox && chmod 4755 ~#{node['chromium']['user']}/#{node['chromium']['expand']}/chrome_sandbox"
 end
