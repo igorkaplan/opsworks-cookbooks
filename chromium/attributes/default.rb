94182640eb6dd3a632e941ed95fae6ea3459d4b2
Greg Cochard <gcochard@users.noreply.github.com>
2015-03-31 16:51:01 -0700

    moving chrome executable name into variable

--- chromium/attributes/default.rb
+++ chromium/attributes/default.rb
@@ -1,4 +1,6 @@
 default['chromium']['user'] = "deploy"
 default['chromium']['expand'] = "chromium"
 default['chromium']['symlink'] = "chromium"
+default['chromium']['chrome'] = "chrome"
+default['chromium']['chrome_sandbox'] = "chrome_sandbox"
 default['chromium']['location'] = "https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux%2F308448%2Fchrome-linux.zip?generation=1418688583562000&alt=media"
