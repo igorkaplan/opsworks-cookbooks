09f7b579f19982974057c98e4db6d128b68526f5
Greg Cochard <gcochard@users.noreply.github.com>
2015-04-15 12:28:37 -0700

    workaround for oom errors, kill chromium and retry

--- chromium/recipes/default.rb
+++ chromium/recipes/default.rb
@@ -2,22 +2,32 @@ remote_file "/home/#{node['chromium']['user']}/archive.zip" do
   source node['chromium']['location']
 end
 
+execute "kill running instances" do
+  command "killall #{node['chromium']['symlink']}"
+  retries 5
+end
+
 execute "remove target directory in preparation" do
   command "rm -rf ~#{node['chromium']['user']}/#{node['chromium']['expand']}"
+  retries 5
 end
 
 execute "unzip chromium" do
   command "unzip -d ~#{node['chromium']['user']}/expandtemp ~#{node['chromium']['user']}/archive.zip && mv ~#{node['chromium']['user']}/expandtemp/* ~#{node['chromium']['user']}/#{node['chromium']['expand']} && rm ~#{node['chromium']['user']}/archive.zip && rm -rf ~#{node['chromium']['user']}/expandtemp"
+  retries 5
 end
 
 execute "symlink chromium" do
   command "rm -f /usr/bin/#{node['chromium']['symlink']} && ln -s ~#{node['chromium']['user']}/#{node['chromium']['expand']}/#{node['chromium']['chrome']} /usr/bin/#{node['chromium']['symlink']}"
+  retries 5
 end
 
-execute "chown/chmod chromium" do
+execute "chown chromium" do
   command "chown -R #{node['chromium']['user']} ~#{node['chromium']['user']}/#{node['chromium']['expand']}"
+  retries 5
 end
 
-execute "chown/chmod chrome_sandbox" do
+execute "chown/chmod sandbox" do
   command "chown root:root ~#{node['chromium']['user']}/#{node['chromium']['expand']}/#{node['chromium']['chrome_sandbox']} && chmod 4755 ~#{node['chromium']['user']}/#{node['chromium']['expand']}/#{node['chromium']['chrome_sandbox']}"
+  retries 5
 end
