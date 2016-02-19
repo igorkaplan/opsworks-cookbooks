871ad8139e3bbd352adf1e605c8d09a41e4d3897
ikaplan <igor.kaplan@viasat.com>
2016-02-19 12:19:47 -0800

    Open ports 443 and 80 on input

--- opsworks_custom_cookbooks/recipes/iptables.rb
+++ opsworks_custom_cookbooks/recipes/iptables.rb
@@ -10,6 +10,12 @@ iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
 iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -o eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
+
 iptables -A INPUT -i eth0 -p tcp -s 10.3.0.0/25 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
