cb3b4c48023a6a18e9d912d64544174a336609ea
ikaplan <igor.kaplan@viasat.com>
2016-02-22 12:49:13 -0800

    Use environment variable for subnet

--- opsworks_custom_cookbooks/recipes/iptables.rb
+++ opsworks_custom_cookbooks/recipes/iptables.rb
@@ -1,9 +1,10 @@
 bash "configure_iptables" do
   code <<-EOH
+    echo "Subnet: $SUBNET_CIDR" > /tmp/output.txt
     iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -F
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -F
 
     iptables -P INPUT DROP
 iptables -P FORWARD DROP
@@ -19,7 +20,7 @@ iptables -A OUTPUT -o eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACC
 iptables -A INPUT -i eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
 
-iptables -A INPUT -i eth0 -p tcp -s 10.3.0.0/25 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -s $SUBNET_CIDR -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp -m state --state ESTABLISHED -j ACCEPT
 
     iptables -A OUTPUT -o eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
